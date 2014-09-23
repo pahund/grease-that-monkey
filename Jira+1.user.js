fabb71b2120c2bfb654c2f298fb385672f19ccc1
Martina Freundorfer <mfreundorfer@team.mobile.de>
2014-09-23 15:17:34 +0200

    Plugin to see previous or next JIRA Tickets according to current Ticket number

---
+++ Jira+1.user.js
@@ -0,0 +1,37 @@
+// ==UserScript== 
+// @name Jira +1 
+// @namespace Jira 
+// @include https://jira.corp.mobile.de/jira/browse/* 
+// @version 2 
+// @grant none 
+// ==/UserScript== 
+AJS.$(document).ready(function() { 
+    
+    AJS.$(".toolbar-split-right").append("<ul class='toolbar-group pluggable-ops'><li class='toolbar-item'><a id='minusOne' title='Den vorherigen Vorgang aufrufen' class='toolbar-trigger' rel='nofollow' href='#'><span class='icon icon-previous'></span>Previous Ticket</a></li><li class='toolbar-item'><a id='plusOne' title='Den nachfolgenden Vorgang aufrufen' class='toolbar-trigger' rel='nofollow' href='#'>Next Ticket<span class='icon icon-next'></span></a></li></ul>"); 
+    
+    function switchPage(direction) {
+        var newURL,  
+        	help,  
+	        number;  
+        help = window.location.href.split("-"); 
+        if (help.length >= 2) {
+            if(direction === "+1") { 
+	        	number = parseInt(help[1], 10) +1;         	
+            } else {
+               	number = parseInt(help[1], 10) -1;
+            }
+        	newURL = help[0] + "-" + number;  
+        	window.location.href = newURL; 
+        } else {
+		    console.log("Something went wrong...");
+        }
+    };
+        
+    AJS.$("#plusOne").click(function () { 
+	    switchPage("+1");
+    }); 
+     
+    AJS.$("#minusOne").click(function () { 
+    	switchPage("-1");
+    });    
+});
